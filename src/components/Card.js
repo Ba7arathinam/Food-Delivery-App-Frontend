a54adabb7f6bfe1b0a00f0d8bd8bd2faa05aa3cf
Ba7arathinam <bala01225@gmail.comm>
2023-07-17 16:19:43 +0530

    do changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -42,18 +42,18 @@ export default function Card(props) {
     console.log(new Date())
     if (food !== '') {
       if (food.size === size) {
-        await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
+        await dispatch({ type: "UPDATE", id: props.foodItem._id, price: props.finalPrice, qty: qty })
         return
       }
       else if (food.size !== size) {
-        await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size,img: props.ImgSrc })
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: props.finalPrice, qty: qty, size: size,img: props.ImgSrc })
         console.log("Size different so simply ADD one more to the list")
         return
       }
       return
     }
 
-    await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size })
+    await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: props.finalPrice, qty: qty, size: size })
 
 
     // setBtnEnable(true)
